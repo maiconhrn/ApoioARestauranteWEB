b15fb93a34d64bfe37f85bfc173cb9be8a05b3d3
maiconhrn <maiconhrn@hotmail.com>
2019-10-05 13:58:48 -0300

    Estrutura de arquivos inicial

---
+++ src/main/java/br/uem/apoioarestaurante/views/LoginView.java
@@ -0,0 +1,11 @@
+package br.uem.apoioarestaurante.views;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class LoginView implements Serializable {
+
+}
