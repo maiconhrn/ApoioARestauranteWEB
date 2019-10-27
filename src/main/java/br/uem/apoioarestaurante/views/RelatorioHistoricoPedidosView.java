8fb23dfd1395766c8ceaea8f8ab4911f61a62009
maiconhrn <maiconhrn@hotmail.com>
2019-10-27 17:07:16 -0300

    Adiciona Telas Views de Relatorios
    Movimentacao de Estoque e Historico de Pedidos

---
+++ src/main/java/br/uem/apoioarestaurante/views/RelatorioHistoricoPedidosView.java
@@ -0,0 +1,30 @@
+package br.uem.apoioarestaurante.views;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.Date;
+
+@Named
+@ViewScoped
+public class RelatorioHistoricoPedidosView implements Serializable {
+
+    private Date beginFilter;
+    private Date endFilter;
+
+    public Date getBeginFilter() {
+        return beginFilter;
+    }
+
+    public void setBeginFilter(Date beginFilter) {
+        this.beginFilter = beginFilter;
+    }
+
+    public Date getEndFilter() {
+        return endFilter;
+    }
+
+    public void setEndFilter(Date endFilter) {
+        this.endFilter = endFilter;
+    }
+}
