43da59a8f2d512e612e66aba9aeff6dce6e58660
Gabriel Conejo <gabriel.conejo@yahoo.com.br>
2020-01-13 02:55:40 -0300

    Criação de testes caixa branca Caminhos Independentes login

---
+++ src/main/test/br/uem/apoioarestaurante/models/UsuarioModelCaminhosIndependetesTest.java
@@ -0,0 +1,57 @@
+package br.uem.apoioarestaurante.models;
+
+import br.uem.apoioarestaurante.metadata.entities.Usuario;
+import br.uem.apoioarestaurante.utils.HibernateUtil;
+import br.uem.apoioarestaurante.views.UsuariosView;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Collections;
+import static br.uem.apoioarestaurante.utils.TestUtil.*;
+import static org.junit.Assert.assertEquals;
+
+
+public class UsuarioModelCaminhosIndependetesTest {
+
+    private Permissao permissao;
+    private GrupoPermissao grupoPermissao;
+    private Usuario usuario;
+    private UsuariosView usuariosView;
+    private String testa;
+
+    @Before
+    public void setUp() throws ParseException {
+        permissao = createPermissao();
+        grupoPermissao = createGrupoPermissao(Collections.singletonList(permissao));
+        usuario = createUser(grupoPermissao);
+        usuariosView = new UsuariosView();
+    }
+
+    @After
+    public void clear() {
+        HibernateUtil.closeSessionFactory();
+    }
+
+    @Test
+    public void autenticacaoCaso1() throws Exception {
+        this.testa = "";
+        assertEquals    ("",
+                        testa);
+    }
+
+    @Test
+    public void autenticacaoCaso2() throws Exception {
+        String testa = (usuariosView.envia("gabriel", "123"));
+        assertEquals    ("/restricted/home.xhtml",
+                        testa);
+    }
+
+    @Test
+    public void autenticacaoCaso3() throws Exception {
+        String testa = usuariosView.envia("gabriel", "abc");
+        assertEquals    ("",
+                         testa);
+    }
+}
